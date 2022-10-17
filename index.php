f4c6281fa96ae1a7f77909fe822986dc0f085acf
charon <tsrn48644978extra@gmail.com>
2022-10-17 08:59:50 +0800

    修改練習

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>基礎九九乘法表</title>
+    <title>乘法表</title>
 </head>
 <body>
 <?php 
-for($j=1;$j<=9;$j++){
+for($j=1;$j<=19;$j++){
     echo '<tr>';
-    for($i=1;$i<=9;$i++){
+    for($i=1;$i<=19;$i++){
         echo "<td> $j X $i =" . ($j*$i) . "</td>";
      }
     echo "</tr>";
